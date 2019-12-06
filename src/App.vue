e209dd81ef133f436048b0dea6c85b82746f3db9
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2019-12-06 22:25:16 +0200

    84 (Vue Router)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-
+    <router-view></router-view>
   </div>
 </template>
 
+<script>
+import HomePage from './components/HomePage'
+import AboutUsPage from './components/AboutUsPage'
+export default {
+  components:{
+    HomePage,
+    AboutUsPage
+  }
+}
+</script>
 
 <style >
 
