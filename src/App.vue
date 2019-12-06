26858b00175b6c389cb90ff9746cc671df50b4fc
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2019-12-07 00:01:00 +0200

    87 (في ملف خاصroutesفصل ال)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
- <ul class="nav nav-pills">
-      <li class="nav-item">
-        <router-link to="/" class="nav-link" active-class="active" exact href="#">Home</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link to="/about" class="nav-link" active-class="active" href="#">About Us</router-link>
-      </li>
-    </ul>
+       <NavBar/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import NavBar from "./components/NaveBar"
 
 export default {
   components:{
-
+    NavBar
   }
 }
 </script>
---
+++ src/components/NaveBar.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <router-link to="/" class="nav-link" active-class="active" exact href="#">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/about" class="nav-link" active-class="active" href="#">About Us</router-link>
+      </li>
+    </ul>
+</template>
